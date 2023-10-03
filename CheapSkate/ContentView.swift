90628dc9ef790a3531b98f2b80f236fff78b13e3
Isaac Boothe <iboothe@uncc.edu>
2023-10-03 15:40:21 -0400

    Initial commit

--- CheapSkate/ContentView.swift
+++ CheapSkate/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("CheapSkate Project")
             Text("Group14")
+            Text(" ")
         }
         .padding()
     }
