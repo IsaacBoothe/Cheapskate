8c44e10d49b7ff40b3b6e5d649a13b5bc944f07e
Isaac Boothe <iboothe@uncc.edu>
2023-10-03 15:39:12 -0400

    Initial Commit

---
+++ CheapSkate/CheapSkateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheapSkateApp.swift
+//  CheapSkate
+//
+//  Created by Isaac Boothe on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CheapSkateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheapSkate/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  CheapSkate
+//
+//  Created by Isaac Boothe on 10/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("CheapSkate Project")
+            Text("Group14")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
