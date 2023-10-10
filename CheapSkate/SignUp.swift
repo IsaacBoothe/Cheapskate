0f1a01b765fbad7ce138f753e015a31d71459e51
Bmclaughlin02 <bdmclaughlin02@gmail.com>
2023-10-10 09:26:13 -0400

    Began to add sign up page

---
+++ CheapSkate/SignUp.swift
@@ -0,0 +1,28 @@
+//
+//  SignUp.swift
+//  CheapSkate
+//
+//  Created by Brennan McLaughlin on 10/10/23.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("CheapSkate Project")
+            Text("Group14")
+            Text(" ")
+        }
+        .padding()
+    }
+}
+
+struct SignUp_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
