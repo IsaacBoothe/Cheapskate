9102254367ef7917e61155a08ada5ea8ea4295ae
Bmclaughlin02 <bdmclaughlin02@gmail.com>
2023-10-10 10:17:21 -0400

    Created basic signup page

--- CheapSkate/SignUp.swift
+++ CheapSkate/SignUp.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct SignUp: View {
+    @State var userName: String = ""
+    @State var email: String = ""
+    @State var password: String = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("CheapSkate Project")
-            Text("Group14")
             Text("Sign Up Page")
+            
+            TextField("Username", text: $userName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button(
+                action: {
+                    // did tap
+                },
+                label: { Text("Sign Up") }
+            )
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(5)
         }
         .padding()
     }
@@ -23,6 +47,6 @@ struct SignUp: View {
 
 struct SignUp_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SignUp()
     }
 }
