f8fc9fc998b579a8a1cba798e4b9da52d7cd4891
Isaac Boothe <iboothe@uncc.edu>
2023-10-11 00:12:08 -0400

    Configured Firebase

--- CheapSkate/CheapSkateApp.swift
+++ CheapSkate/CheapSkateApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct CheapSkateApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("Firebase Configured!!")
+        
+        return true
     }
 }
