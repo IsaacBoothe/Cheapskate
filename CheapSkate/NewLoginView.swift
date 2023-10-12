6f3ae585d83dd1b511d02f0464088aad4f12c210
itk1m <143761576+itk1m@users.noreply.github.com>
2023-10-12 19:42:17 -0400

    Add files via upload

---
+++ CheapSkate/NewLoginView.swift
@@ -0,0 +1,47 @@
+//
+//  NewLoginView.swift
+//  CheapSkate
+//
+//  Created by Isaac Kim on 10/12/23.
+//
+
+import SwiftUI
+
+struct NewLoginView: View {
+    @State var userName: String = ""
+    @State var emailAddress: String = ""
+    @State var password: String = ""
+    var body: some View {
+        Text("New Login Credenntials Page").padding()
+        VStack(content: {
+            Text("Placeholder")
+            TextField("Enter your username", text: $userName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Text("Change user email address")
+            TextField("Enter your new email address", text: $emailAddress)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Text("Enter your new password")
+            SecureField("Enter your new password", text: $password).textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Button(
+                action: {
+                // did tap
+                },
+                label: { 
+                    Text("Update") }
+                )
+                .padding()
+                .background(Color.white)
+                .cornerRadius(5)
+            
+        })
+        .padding()
+        .background(Color.green)
+    }
+}
+
+#Preview {
+    NewLoginView()
+}
