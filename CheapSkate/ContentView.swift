be5d5b5eb1bf563cf350a670ba1f75cd14a3bbbc
jaredf96 <jfulk44@gmail.com>
2023-10-31 16:05:42 -0400

    password reset finished

--- CheapSkate/ContentView.swift
+++ CheapSkate/ContentView.swift
@@ -9,15 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("CheapSkate Project")
-            Text("Group14")
-            Text(" ")
-        }
-        .padding()
+        PasswordResetView()
     }
 }
 
---
+++ CheapSkate/git test.swift
@@ -0,0 +1,10 @@
+//
+//  git test.swift
+//  CheapSkate
+//
+//  Created by Jared Fulk on 10/12/23.
+//
+
+import Foundation
+// Push test
+// New change
