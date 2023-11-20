7d62decf65a63a1ce867399100b4ac2a817d044c
Bmclaughlin02 <bdmclaughlin02@gmail.com>
2023-11-19 21:00:32 -0500

    Added like button to article page

--- CheapSkate/Articles/ArticlePage.swift
+++ CheapSkate/Articles/ArticlePage.swift
@@ -10,7 +10,24 @@ import SwiftUI
 struct ArticlePage: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        LikeButton()
     }
+}
+
+struct LikeButton: View {
+    @State private var isLiked = false
+    
+    var body: some View {
+            Button(action: {
+                // Toggle the value of the `isLiked` variable when the button is tapped
+                self.isLiked.toggle()
+            }) {
+                // Use an image or label to indicate that the button is a "like" button
+                Image(systemName: isLiked ? "hand.thumbsup.fill" : "hand.thumbsup")
+                Text(isLiked ? "Liked" : "Like")
+            }
+        }
 }
 
 struct ArticlePage_Previews: PreviewProvider {
