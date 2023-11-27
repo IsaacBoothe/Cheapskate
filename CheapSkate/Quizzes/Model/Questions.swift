08c87b3a01989873155db574b5e45073e4b01b39
itk1m <isaactaekim987@gmail.com>
2023-11-27 18:19:58 -0500

    Questions model commit

---
+++ CheapSkate/Quizzes/Model/Questions.swift
@@ -0,0 +1,26 @@
+//
+//  Questions.swift
+//  CheapSkate
+//
+//  Created by Isaac Kim on 11/27/23.
+//
+
+import SwiftUI
+
+// MARK: Quiz Questions Codable Model
+struct Questions: Identifiable, Codable {
+    var id: UUID = .init()
+    var answer: String
+    var choice: [String]
+    var question: String
+    
+    // for UI State Updates
+    var tappedAnswer: String = ""
+    
+    enum CodingKeys: CodingKey{
+        case answer
+        case choice
+        case question
+    }
+}
+
